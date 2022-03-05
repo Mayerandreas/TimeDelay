faf6127ddbe48e1fe19292c8c8634504eea197b8
Mayer Andreas <l1022@schueler.ms-muc.de>
2022-03-05 17:06:38 +0100

    Übung05032201

--- TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
+++ TestTimeDelay/main/examples/EXAMPLE_TON/main.cpp
@@ -12,6 +12,7 @@
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "StandardLib.h"
+#include "BasicLib.h"
 
 #define LOG_LOCAL_LEVEL ESP_LOG_INFO
 #include "esp_log.h"
@@ -21,8 +22,11 @@ static const char * const TAG = "EXAMPLE_TON";
 
 #define BUTTON_I1 GPIO_NUM_26        // Pin 26.
 #define BUTTON_I2 GPIO_NUM_32
+#define BUTTON_I3 GPIO_NUM_39
 #define GPIO_Q1 GPIO_NUM_19            // Pin 19.
 #define GPIO_Q2 GPIO_NUM_23
+#define GPIO_Q3 GPIO_NUM_33
+
 
 
 /* Inside .cpp file, app_main function must be declared with C linkage */
@@ -40,22 +44,31 @@ extern "C" void app_main(void)
     */
     gpio_reset_pin(GPIO_Q1);
     gpio_reset_pin(GPIO_Q2);
+    gpio_reset_pin(GPIO_Q3);
     gpio_reset_pin(BUTTON_I1);
     gpio_reset_pin(BUTTON_I2);
+    gpio_reset_pin(BUTTON_I3);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(GPIO_Q1, GPIO_MODE_OUTPUT);
     gpio_set_direction(GPIO_Q2, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_Q3, GPIO_MODE_OUTPUT);
     gpio_set_direction(BUTTON_I1, GPIO_MODE_INPUT);
     gpio_set_direction(BUTTON_I2, GPIO_MODE_INPUT);
+    gpio_set_direction(BUTTON_I3, GPIO_MODE_INPUT);
     gpio_set_level(GPIO_Q1, 0); //set to 0 at Reset.
     gpio_set_level(GPIO_Q2, 0); //set to 0 at Reset.
+    gpio_set_level(GPIO_Q3, 0); //set to 0 at Reset.
 
 
 
     TON TON1;
     TON1.PT =1000;
     TON TON2;
-    TON2.PT =1000;
+        TON2.PT =1000;
+
+
+
+    TOGGLE TOGGLE1;
 
     while (true) // Endlos-Schleife
     {
@@ -64,14 +77,19 @@ extern "C" void app_main(void)
 
         bool I2 = not gpio_get_level(BUTTON_I2);
 
+        bool I3 = not gpio_get_level(BUTTON_I3);
+
+        TOGGLE1.RST = I3;
 
         // den I1 an TON1 uebergeben, und TON1 aufrufen
         TON1(I1);
         TON2(I2);
+        TOGGLE1(TON1.Q);
 
         // Ausgaenge setzen
-        gpio_set_level(GPIO_Q1, TON1.Q);
+        gpio_set_level(GPIO_Q1, TOGGLE1.Q);
         gpio_set_level(GPIO_Q2, TON2.Q);
+
 
         // 100ms warten  = Intervallzeit des Tasks
         vTaskDelay(100 / portTICK_PERIOD_MS); // 100ms cycle for Test.
